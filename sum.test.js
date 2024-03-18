f91380d27cad1e9ba6a10b1ec851a0deb2010355
Krystle <krystle@Krystles-MBP.lan>
2024-03-17 19:21:38 -0700

    created tests file for challenges

---
+++ sum.test.js
@@ -0,0 +1,12 @@
+const sum = require('./Arrays/1.BasicLoop');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+
+const total = require('./Arrays/goodVsEvil');
+
+test('googToal > evilTotal total equal', () => {
+  expect(total())
+})
