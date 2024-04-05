8f0b1430266fcd1659d51d555056c2698f80c9f8
Krystle <krystle@Krystles-MBP.lan>
2024-04-05 11:46:56 -0700

    Test 2 is working

---
+++ Arrays/mergeStr.js
@@ -0,0 +1,12 @@
+function mergeStrings(word1, word2) {
+  let word3 = word1 + word2
+
+  return word3
+    
+};
+
+module.exports = mergeStrings; 
+
+      
+
+        
--- sum.test.js
+++ sum.test.js
@@ -1,12 +1,21 @@
 const sum = require('./Arrays/1.BasicLoop');
+const mergeStrings = require('./Arrays/mergeStr');
+const longestPrefix = require('./Strings/longestPrefix');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
 
-const total = require('./Arrays/goodVsEvil');
 
-test('googToal > evilTotal total equal', () => {
-  expect(total())
+
+test('word1 + word2 equal word1word2', () => {
+  const word1 = "abd"
+  const word2 = 'hg'
+  const word3 = word1 + word2
+  expect(mergeStrings(word1, word2)).toBe(word3)
 })
+
+// test(strs, () => {
+//   longestPrefix
+// })
